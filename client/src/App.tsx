b1acc7b7ea327e8b880a8e092caf662d0760a544
Alex CB <ketepincho@gmail.com>
2022-07-06 16:13:57 +0200

    Removed useless files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,5 @@
-// import React from "react";
-// We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
-// We import all the components we need in our app
 import Navbar from "./components/navbar";
-// import RecordList from "./components/recordList";
-// import Edit from "./components/edit";
-// import Create from "./components/create";
 import MainForm from "./components/mainForm";
 
 const App = () => {
