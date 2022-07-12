c60c90d5d39940c304008ee058c31d6de3c35611
Alex CB <ketepincho@gmail.com>
2022-07-12 18:00:20 +0200

    Added reportWebVitals

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import App from "./App";
-
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
@@ -14,3 +15,8 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>,
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
