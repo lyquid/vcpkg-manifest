7b953009324a5b06e8caeb34d8eeac8dcdbf3c02
Alex CB <ketepincho@gmail.com>
2022-07-18 00:03:59 +0200

    Added the client to production

--- server/src/routes/record.ts
+++ server/src/routes/record.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import { database } from "../db/conn";
 
 // recordRoutes is an instance of the express router.
@@ -9,9 +10,13 @@ const recordRoutes = express.Router();
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
+recordRoutes.route("/").get((req: express.Request, res: express.Response) => {
+  const buildPath = path.join(__dirname, '../build', 'index.html');
+  res.sendFile(buildPath);
+});
+
 // This section will help you get a list of all the records.
 recordRoutes.route("/libraries").get((req: express.Request, res: express.Response) => {
-  // let db_connect = dbo.getDb("cluster0");
   let db_connect = database;
   db_connect
     .collection("restaurants")
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express from "express";
+import path from "path";
 import { connectToDatabase } from "./db/conn";
 
 require("dotenv").config({ path: "./config.env" });
@@ -9,6 +10,7 @@ const app: express.Application = express();
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+app.use(express.static(path.join(__dirname, 'build')));
 app.listen(port, () => {
   connectToDatabase();
   console.log(`Server is running on port: ${port}`);
