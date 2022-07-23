ce5b02962766963c8e2db15cfa4c91ae80b0fb6d
Alex CB <ketepincho@gmail.com>
2022-07-23 02:21:11 +0200

    Contact info first step

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -19,6 +19,8 @@ export default function TopBar() {
 
           <LanguagePicker/>
 
+          {/* <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="alejandro-castillo-blanco-23180523b" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://es.linkedin.com/in/alejandro-castillo-blanco-23180523b?trk=profile-badge">Alejandro Castillo Blanco</a></div> */}
+
         </Toolbar>
       </Container>
     </AppBar>
