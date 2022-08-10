dd136b2101a4f72ba4455302d7666d85f5427ca5
Alex CB <ketepincho@gmail.com>
2022-08-10 17:42:07 +0200

    Removed unneeded attributes form github icon

--- client/src/components/header/GitHubIcon.tsx
+++ client/src/components/header/GitHubIcon.tsx
@@ -1,24 +1,18 @@
 import { useTranslation } from 'react-i18next';
-import { GitHub } from "@mui/icons-material";
-import { Tooltip, Typography } from "@mui/material";
+import { GitHub } from '@mui/icons-material';
+import { Tooltip, Typography } from '@mui/material';
 
 export default function GitHubIcon() {
   const { t } = useTranslation();
   return(
     <Tooltip title={t('topBar.github')}>
       <Typography
-        variant="h6"
-        noWrap
-        component="a"
-        href="https://github.com/lyquid/vcpkg-manifest"
+        component='a'
+        href='https://github.com/lyquid/vcpkg-manifest'
         sx={{
           mr: 2,
           display: 'flex',
-          fontFamily: 'monospace',
-          fontWeight: 700,
-          letterSpacing: '.3rem',
           color: 'inherit',
-          textDecoration: 'none'
         }}
       >
         <GitHub/>
