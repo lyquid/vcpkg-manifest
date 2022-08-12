573b2e4868876571aab24d0a742152a055363543
Alex CB <ketepincho@gmail.com>
2022-08-12 16:49:23 +0200

    Improved server shutdown

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,13 +7,17 @@ import path from 'path';
 require('dotenv').config({ path: './config.env' });
 const port = process.env.PORT || 5000;
 
-const app: express.Application = express();
+const app = express();
 app.use(compression());
 app.use(cors());
 app.use(express.json());
 app.use(require('./routes/record'));
 app.use(express.static(path.join(__dirname, 'build')));
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // connectToDatabase();
   console.log(`Server is running on port: ${port}`);
+});
+
+process.on('SIGTERM', () => {
+  server.close(() => console.log('Server shutting down.'));
 });
