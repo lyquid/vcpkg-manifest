842963ef337d668393e9f598a6faecf16fca38c8
Alex CB <ketepincho@gmail.com>
2022-08-23 23:52:06 +0200

    Added rate-limit. Fixes #9 and #10

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,18 @@ import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
 import path from 'path';
+import rateLimit from 'express-rate-limit'
 // import { connectToDatabase } from './db/conn';
 
 require('dotenv').config({ path: './config.env' });
 const port = process.env.PORT || 5000;
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100,                 // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true,    // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false      // Disable the `X-RateLimit-*` headers
+});
 
 const app = express();
 app.use(compression());
@@ -13,6 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(require('./routes/record'));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(limiter);
 const server = app.listen(port, () => {
   // connectToDatabase();
   console.log(`Server is running on port: ${port}`);
